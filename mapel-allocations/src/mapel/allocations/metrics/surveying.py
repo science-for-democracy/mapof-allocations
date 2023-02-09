c8d16c89c291632d0877f0ff5714795c2e1853fc
Andrzej Kaczmarczyk <andrzej.kaczmarczyk@agh.edu.pl>
2023-02-09 09:57:30 +0100

    Fixing round robin.

--- mapel-allocations/src/mapel/allocations/metrics/surveying.py
+++ mapel-allocations/src/mapel/allocations/metrics/surveying.py
@@ -8,10 +8,9 @@ __distances = { "ideal": idealdist.ideal_distance,
 
 def get_distance(left_task, right_task, distance_id):
     """ Return: distance between instances, (if applicable) optimal matching """
-    try: 
-      logger.debug(f"Getting distance: {distance_id}")
-      distance_function =  __distances.get(distance_id, None)
-    except KeyError:
+    logger.debug(f"Getting distance: {distance_id}")
+    distance_function =  __distances.get(distance_id, None)
+    if not distance_function:
       logger.warning(f"No distance with id: {distance_id}")
       return (0, None)
     return distance_function(left_task, right_task)
